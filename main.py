7d55fc73051200b65f40e43da4a5bd291dc3de16
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-01 01:39:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+from tkinter import *
 import math, random
 
-def getOTP(n):
+def getOTP():
     digits = '0123456789'
-    otp=""
-    for i in range(n):
+    otp = ''
+    for i in range(int(r1var.get())):
         otp += digits[math.floor(random.random() * 10)]
-    return f'your {n} digit otp is {otp}'
+    otpvar.set(otp)
 
-if __name__ == "__main__":
-    while True:
-        otplen = int(input('Enter otp length : '))
-        print(f'{getOTP(otplen)}')
+root = Tk()
+root.title('OTP Generator')
+root.geometry('240x240')
+frm1 = Frame(root)
+r1var = StringVar()
+r1var.set(4)
+Label(frm1,text='Size',font='arial 9 bold').pack(side=LEFT,padx=8)
+r1 =Radiobutton(frm1,text='Four', variable=r1var, value=4,padx='5',bg='orange').pack(side=LEFT)
+r2 =Radiobutton(frm1,text='Six', variable=r1var, value=6,padx='5',bg='orange').pack(side=LEFT)
+frm1.pack(pady=20)
+# frm2 = Frame(root)
+Button(root,text='get OTP',font='arial 8 bold', command=getOTP,activebackground='yellow').pack(padx=5)
+# frm2.pack()
+otpvar = IntVar()
+Entry(root,textvariable=otpvar,font='arial 10 bold', width='18').pack(pady=10)
+Button(root,text='Exit',font='arial 8 bold',command=exit,activebackground='red').pack(padx=5)
+root.mainloop()
